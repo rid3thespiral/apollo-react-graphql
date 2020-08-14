8e8745a5fc0d00ffa072eb61d4a1f960b82f8138
rid3thespiral <davidebattaglia1993@gmail.com>
2020-08-14 14:26:20 +0200

    connect react app with server2.js graphql server

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import ApolloClient from 'apollo-boost';
 import './App.css';
+import { ApolloProvider } from "react-apollo";
+import Courses from './components/Courses';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+const App = () => (
+  <ApolloProvider client = {client}>
+    <div>
+      <Courses/>
     </div>
-  );
-}
+  </ApolloProvider>
+)
 
 export default App;
